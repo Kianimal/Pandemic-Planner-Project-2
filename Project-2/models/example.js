37c96eaef2581e6c120736f037670705a5ba085a
Kianimal <spoono17@gmail.com>
2020-05-12 18:23:54 -0500

    Added view for user.handlebars, user search through apiRoutes works. Currently pulls user id, username, email, and score (not a calculated value but a placeholder).

--- Project-2/models/example.js
+++ Project-2/models/example.js
@@ -1,7 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var User = sequelize.define("User", {
+    username: DataTypes.STRING,
+    userpass: DataTypes.STRING,
+    email: DataTypes.STRING,
+    score: DataTypes.INTEGER
   });
-  return Example;
+  return User;
 };
