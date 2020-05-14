bb3dc8bdc710e84ab3f2dcee041d2f6e8290532c
Kianimal <spoono17@gmail.com>
2020-05-13 19:15:14 -0500

    users API route currently working. Adjusted file structure slightly. Changed handlebars version to 4.5.0.

--- models/schema.sql
+++ models/schema.sql
@@ -4,35 +4,26 @@ CREATE database prepDB;
 USE prepDB;
 
 CREATE TABLE users(
-  user_id INT AUTO_INCREMENT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
   username VARCHAR(255),
-  user_pass VARCHAR(255),
-  score_val INT(10) NOT NULL,
-  self_scored_1 INT(10),
-  self_scored_2 INT(10),
-  self_scored_3 INT(10),
-  self_scored_4 INT(10),
-  self_scored_5 INT(10),
-  inventory_1 INT(10),
-  inventory_2 INT(10),
-  inventory_3 INT(10),
-  inventory_4 INT(10),
-  inventory_5 INT(10),
-  evac_plan BOOLEAN NOT NULL,
-  trade_status BOOLEAN NOT NULL,
-  PRIMARY KEY (user_id)
+  userpass VARCHAR(255),
+  email VARCHAR(255),
+  scoreVal INT(10),
+  -- shelter INT(10),
+  -- evac INT(10),
+  -- vehicle INT(10),
+  -- cash INT(10),
+  -- food INT(10),
+  -- medication INT(10),
+  -- sanitizer INT(10),
+  -- toiletries INT(10),
+  -- masks INT(10),
+  -- water INT(10),
+  PRIMARY KEY (id)
 );
 
-INSERT INTO users (username, user_pass, score_val, self_scored_1, self_scored_2,
-self_scored_3, self_scored_4, self_scored_5, inventory_1, inventory_2, inventory_3,
-inventory_4, inventory_5, evac_plan, trade_status)VALUES("Test","TESTPW",1,1,1,1,1,1,1,1,1,1,1,FALSE,FALSE);
-
-INSERT INTO users (username, user_pass, score_val, self_scored_1, self_scored_2,
-self_scored_3, self_scored_4, self_scored_5, inventory_1, inventory_2, inventory_3,
-inventory_4, inventory_5, evac_plan, trade_status)VALUES("Test2","TESTPW",3,4,2,2,1,5,1,3,4,2,5,TRUE,FALSE);
-
-INSERT INTO users (username, user_pass, score_val, self_scored_1, self_scored_2,
-self_scored_3, self_scored_4, self_scored_5, inventory_1, inventory_2, inventory_3,
-inventory_4, inventory_5, evac_plan, trade_status)VALUES("Test3","TESTPW",5,5,5,5,5,5,5,5,5,5,5,TRUE,TRUE);
+INSERT INTO users (username, userpass, scoreVal, shelter, evac,
+vehicle, cash, food, medication, sanitizer, toiletries,
+masks, water)VALUES("Test","testpw",1,1,1,1,1,1,1,1,1,1,1,FALSE,FALSE);
 
 SELECT * FROM users;
