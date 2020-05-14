79dca70112020630aaef65a55d100bf8dc25d50c
fitzpaky <kfitzpatrick44@gmail.com>
2020-05-13 19:36:42 -0700

    adding sign up api and adding updated nav links for handlebars

--- server.js
+++ server.js
@@ -23,8 +23,6 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-// require("./routes/userApiRoutes")(app);
-// require("./routes/imageApiRoutes")(app);
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
