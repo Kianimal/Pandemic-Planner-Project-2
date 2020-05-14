35870fe393402731b66fbe4908c3f2d81fd37760
fitzpaky <kfitzpatrick44@gmail.com>
2020-05-14 11:25:38 -0700

    adding select for image url

--- public/assets/js/image.js
+++ public/assets/js/image.js
@@ -1,6 +1,6 @@
 // Get references to page elements
-var $image = $(".selected");
-var $submitBtn = $("#submit");
+// var $image = $(".selected");
+var $submitBtn = $("#image_submit");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
@@ -29,25 +29,25 @@ var API = {
 };
 
 // // refreshusers gets new users from the db and repopulates the list
-var refreshImage = function() {
-  API.getImage().then(function(data) {
-    var $imageUrl = data.map(function(image) {
-      var $a = $("<a>").attr("href", "/user/" + user.id);
+// var refreshImage = function() {
+//   API.getImage().then(function(data) {
+//     var $imageUrl = data.map(function(image) {
+//       var $a = $("<a>").attr("href", "/user/" + user.id);
 
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": user.id
-        })
-        .append($a);
+//       var $li = $("<li>")
+//         .attr({
+//           class: "list-group-item",
+//           "data-id": user.id
+//         })
+//         .append($a);
 
-      return $li;
-    });
+//       return $li;
+//     });
 
-    $userList.empty();
-    $userList.append($imageUrl);
-  });
-};
+//     $userList.empty();
+//     $userList.append($imageUrl);
+//   });
+// };
 
 // handleFormSubmit is called whenever we submit a new user
 // Save the new user to the db and refresh the list
@@ -56,7 +56,7 @@ var handleFormSubmit = function(event) {
 
   var image = {
     // eslint-disable-next-line camelcase
-    image_url: $image.val()
+    image_url: icon
   };
 
   console.log(image);
@@ -73,3 +73,4 @@ var handleFormSubmit = function(event) {
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
+
